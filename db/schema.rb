85756e1f0a1b77a4d9a6efc2c6d48dad3b6e95ae
BRIANNA KINCART <briannakincart@gmail.com>
2017-05-01 23:31:45 -0400

    schema change

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,31 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170430224048) do
+ActiveRecord::Schema.define(version: 20170502024618) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.text     "body",         null: false
+    t.integer  "user_id"
+    t.integer  "inventory_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.index ["inventory_id"], name: "index_comments_on_inventory_id", using: :btree
+    t.index ["user_id"], name: "index_comments_on_user_id", using: :btree
+  end
+
+  create_table "inventories", force: :cascade do |t|
+    t.integer  "quantity",                   null: false
+    t.string   "measurement"
+    t.string   "item",                       null: false
+    t.boolean  "available",   default: true, null: false
+    t.integer  "user_id"
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
+    t.index ["user_id"], name: "index_inventories_on_user_id", using: :btree
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name",                                null: false
